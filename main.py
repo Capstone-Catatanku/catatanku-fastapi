935310b3a7b4809ffbf7e0b2a1047f03fd20ed7b
yobby15 <yobby.novialdi15@gmail.com>
2026-05-17 23:41:13 +0700

    Update:  setting system hugging face

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
+import os
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
 from routes.chat import router as chat_router
 from routes.predict import router as predict_router
 
-app = FastAPI(
-    title="Modular OpenRouter Chatbot",
-    description="API Chatbot menggunakan OpenRouter (Gemini) dengan struktur modular.",
-    version="1.0.0"
-)
+settings = {
+    "title": "Catatanku",
+    "description": "API Catatanku untuk memprediksi tabungan, mengklasifikasikan kategori dan chatbot",
+    "version": "1.0.0",
+    "docs_url": None,
+    "redoc_url": None
+}
+
+if os.environ.get("SPACE_ID"):
+    settings["root_path"] = f"/spaces/{os.environ.get('SPACE_ID')}"
+
+app = FastAPI(**settings)
 
 app.include_router(chat_router)
 app.include_router(predict_router)
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    openapi_url = f"{app.root_path}{app.openapi_url}" if app.root_path else app.openapi_url
+    
+    return get_swagger_ui_html(
+        openapi_url=openapi_url,
+        title=app.title + " - Swagger UI",
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
+    )
 
 @app.get("/")
 async def root():
