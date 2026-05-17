2f22b0cb559777155a5f642d7755d87095aa8f66
yobby15 <yobby.novialdi15@gmail.com>
2026-05-18 00:10:59 +0700

    refactor: simplify FastAPI setup by removing Hugging Face root_path workaround

--- main.py
+++ main.py
@@ -1,35 +1,15 @@
-import os
 from fastapi import FastAPI
-from fastapi.openapi.docs import get_swagger_ui_html
 from routes.chat import router as chat_router
 from routes.predict import router as predict_router
 
-settings = {
-    "title": "Catatanku",
-    "description": "API Catatanku untuk memprediksi tabungan, mengklasifikasikan kategori dan chatbot",
-    "version": "1.0.0",
-    "docs_url": None,
-    "redoc_url": None
-}
-
-if os.environ.get("SPACE_ID"):
-    settings["root_path"] = f"/spaces/{os.environ.get('SPACE_ID')}"
-
-app = FastAPI(**settings)
+app = FastAPI(
+    title="Catatanku",
+    description="API Catatanku untuk memprediksi tabungan, mengklasifikasikan kategori dan chatbot",
+    version="1.0.0"
+)
 
 app.include_router(chat_router)
 app.include_router(predict_router)
-
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    openapi_url = f"{app.root_path}{app.openapi_url}" if app.root_path else app.openapi_url
-    
-    return get_swagger_ui_html(
-        openapi_url=openapi_url,
-        title=app.title + " - Swagger UI",
-        swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
-        swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
-    )
 
 @app.get("/")
 async def root():
